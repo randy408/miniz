2b2d7c5339b2913dcba2bf31bb87354bffbfc6f9
Randy <randy408@protonmail.com>
2020-11-24 14:53:13 +0100

    fix build script

--- tests/ossfuzz.sh
+++ tests/ossfuzz.sh
@@ -35,8 +35,8 @@ for f in $(find $SRC -name '*_fuzzer.c'); do
 done
 
 
-# Add .zip input files for the zip fuzzer
+# Add .zip input file for the zip fuzzer
 rm -f $OUT/zip_fuzzer_seed_corpus.zip
-zip $OUT/*.zip $OUT/zip_fuzzer_seed_corpus.zip
+zip $OUT/zip_fuzzer_seed_corpus.zip $OUT/seed_corpus.zip
 
 cp tests/zip.dict $OUT/zip_fuzzer.dict
